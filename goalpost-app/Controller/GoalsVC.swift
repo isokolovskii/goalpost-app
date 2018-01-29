f0b20d5e29e707590e3ac05949caf37205d89031
Ivan Sokolovskii <ivan3177@gmail.com>
2018-01-29 17:33:47 +0300

    Created main UI

---
+++ goalpost-app/Controller/GoalsVC.swift
@@ -0,0 +1,21 @@
+//
+//  GoalsVC.swift
+//  goalpost-app
+//
+//  Created by Иван Соколовский on 29.01.2018.
+//  Copyright © 2018 iSOKOL-DEV. All rights reserved.
+//
+
+import UIKit
+
+class GoalsVC: UIViewController {
+    // Outlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func addGoalPressed(_ sender: Any) {e
+    }
+}
