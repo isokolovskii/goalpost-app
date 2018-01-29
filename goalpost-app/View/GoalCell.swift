a8f474e087b95b26b9788beb6a0014815fbf498c
Ivan Sokolovskii <ivan3177@gmail.com>
2018-01-29 17:52:23 +0300

    Created goal cell to show goals in table

---
+++ goalpost-app/View/GoalCell.swift
@@ -0,0 +1,22 @@
+//
+//  GoalCell.swift
+//  goalpost-app
+//
+//  Created by Иван Соколовский on 29.01.2018.
+//  Copyright © 2018 iSOKOL-DEV. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+    // Outlets
+    @IBOutlet weak var goalDecriptionLbl: UILabel!
+    @IBOutlet weak var goalTypeLbl: UILabel!
+    @IBOutlet weak var goalProgressLbl: UILabel!
+    
+    func configureCell(goalDescription description: String, goalType type: String, goalProgressAmount progress: Int) {
+        goalDecriptionLbl.text = description
+        goalTypeLbl.text = type
+        goalProgressLbl.text = "\(progress)"
+    }
+}
